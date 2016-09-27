8afcb9e9c7cc67b9a103d7c89492e6e75f16df0e
Feng Xi <xifeng@nationsky.com>
2016-09-27 12:33:58 +0800

    install.sh fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname ${SCRIPT})
 
-local install_to="${1:-$HOME}"
+install_to="${1:-$HOME}"
 cp -fr ${SCRIPTPATH}/.bashrc* ${install_to}
 cp -fr ${SCRIPTPATH}/.mybashrc ${install_to}
 cp -fr ${SCRIPTPATH}/.vimrc ${install_to}
