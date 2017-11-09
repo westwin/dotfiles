c690eeb38e7c7629c05995aa9cf155cd98ed08f4
Feng Xi <xifeng@nationsky.com>
2017-11-09 14:17:06 +0800

    feat(dotfiles): add vscode settings and keybindings

--- install.sh
+++ install.sh
@@ -10,3 +10,22 @@ cp -fr ${SCRIPTPATH}/.inputrc ${install_to}
 cp -fr ${SCRIPTPATH}/.tmux* ${install_to}
 
 #source ${HOME}/.bashrc
+
+# install vscode setting
+vscode_user_dir=${install_to}/.config/Code/User
+if [[ -d ${vscode_user_dir} ]]; then
+    if [[ -f ${vscode_user_dir}/settings.json ]]; then
+        # backup
+        mv ${vscode_user_dir}/settings.json ${vscode_user_dir}/settings.json-before-dotfiles-install.backup
+
+        ln -sf ${SCRIPTPATH}/vscode/User/settings.json ${vscode_user_dir}/settings.json
+    fi
+
+    if [[ -f ${vscode_user_dir}/keybindings.json ]]; then
+        # backup
+        mv ${vscode_user_dir}/keybindings.json ${vscode_user_dir}/keybindings.json-before-dotfiles-install.backup
+
+        ln -sf ${SCRIPTPATH}/vscode/User/keybindings.json ${vscode_user_dir}/keybindings.json
+    fi
+fi
+
