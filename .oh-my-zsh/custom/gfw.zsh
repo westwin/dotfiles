6fdf7cc3c204cb8d4c0cf077376ca78db04c7c13
Feng Xi <westwin@21cn.com>
2018-07-10 16:49:12 +0800

    tmux update

--- .oh-my-zsh/custom/gfw.zsh
+++ .oh-my-zsh/custom/gfw.zsh
@@ -1,2 +1,2 @@
-alias fq='export http_proxy="http://127.0.0.1:8118";export https_proxy="http://127.0.0.1:8118"'
-alias nfq='unset http_proxy;unset https_proxy'
+alias fq='export http_proxy="http://127.0.0.1:8118";export https_proxy="http://127.0.0.1:8118"; export HTTP_PROXY="http://127.0.0.1:8118";export HTTPS_PROXY="http://127.0.0.1:8118"'
+alias nfq='unset http_proxy;unset https_proxy;unset HTTPS_PROXY;unset HTTPS_PROXY'
