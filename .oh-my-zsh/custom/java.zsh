3813eaf404954bf65a64401cfe091f525094078a
FengXi <westwin@21cn.com>
2019-01-31 15:55:55 +0800

    vscode for java

--- .oh-my-zsh/custom/java.zsh
+++ .oh-my-zsh/custom/java.zsh
@@ -3,6 +3,7 @@ export ANT_HOME="/usr/local/ant/apache-ant-1.9.6"
 #export JAVA_HOME="/usr/lib/jvm/java-openjdk"
 #export JAVA_HOME="/usr/lib/jvm/java"
 export JAVA_HOME="/usr/java/default"
+export JDK_HOME="/usr/java/default"
 export ECLIPSE_HOME="/home/xifeng/3rd/eclipse/jee-neon/eclipse"
 export GRADLE_HOME="/usr/local/gradle/default/"
 
@@ -10,4 +11,4 @@ export IDEA_HOME="/usr/local/idea"
 
 
 export PATH="$MVN_HOME/bin:$ANT_HOME/bin:$JAVA_HOME/bin:$ECLIPSE_HOME:$GRADLE_HOME/bin:${IDEA_HOME}/bin:$PATH"
-
+alias idea="$HOME/3rd/idea &"
