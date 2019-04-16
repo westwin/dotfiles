0878116c32153c131191031363531c144a2a0176
Feng Xi <fengxi@yufuid.com>
2019-04-16 12:43:12 +0800

    move confidential to private

--- .oh-my-zsh/custom/yufuid.zsh
+++ .oh-my-zsh/custom/yufuid.zsh
@@ -1,6 +1,2 @@
 wk=$HOME/work/yufuid/code
 
-#dev env
-export DB_HOST=nx-dev-idaas-pg.czzoopdyw428.rds.cn-northwest-1.amazonaws.com.cn
-export DB_PORT=5432
-alias db="pgcli -h $DB_HOST -p $DB_PORT -u idaas -W idaas"
