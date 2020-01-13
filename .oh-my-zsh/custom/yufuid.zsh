5d25fa059e66d60edbf56f90917b368d9453f01f
Feng Xi <fengxi@yufuid.com>
2020-01-13 09:38:21 +0800

    update tmux conf

--- .oh-my-zsh/custom/yufuid.zsh
+++ .oh-my-zsh/custom/yufuid.zsh
@@ -4,9 +4,9 @@ oidc=~wk/yufu-vds/oidc
 idaas=~wk/idaas-core
 
 #dev
-portal="http://xifeng.i.yufuid.com:6040/api/v1/tn-0a5a283e76074b23a532c99f6c3a81b9"
-admin="http://xifeng.admin.i.yufuid.com:6020/api/v1/tn-0a5a283e76074b23a532c99f6c3a81b9"
-idp="http://xifeng.idp.i.yufuid.com:6010/api/v1/tn-0a5a283e76074b23a532c99f6c3a81b9"
+portal="https://xifeng.i.yufuid.com/api/v1/tn-0a5a283e76074b23a532c99f6c3a81b9"
+admin="http://xifeng-admin.i.yufuid.com/api/v1/tn-0a5a283e76074b23a532c99f6c3a81b9"
+idp="https://xifeng-idp.i.yufuid.com/api/v1/tn-0a5a283e76074b23a532c99f6c3a81b9"
 
 #admin api
 #curl -b access_token="${TOKEN}" -XPOST "${admin}/objects"" -H 'Content-Type: application/json; charset=UTF-8' --data-binary '{"objectType":"USER","values":{"password":"Yufu@2018","displayName":"dev11","username":"demo1@example.com","primaryMail":"westwin@gmail.com","secondaryMail":"","phoneNum":"","deptId":"","leaderId":""}}'
@@ -16,3 +16,6 @@ idp="http://xifeng.idp.i.yufuid.com:6010/api/v1/tn-0a5a283e76074b23a532c99f6c3a8
 #fq
 #alias fq='export HTTP_PROXY=http://10.34.101.53:8118;export HTTPS_PROXY=http://10.34.101.53:8118'
 #alias nfq='unset HTTP_PROXY; unset HTTPS_PROXY'
+
+#qunhui poc
+alias qh='ssh ec2-user@10.16.1.182 -p 22'
--- .oh-my-zsh/plugins/tmux/tmux.plugin.zsh
+++ .oh-my-zsh/plugins/tmux/tmux.plugin.zsh
@@ -2,9 +2,9 @@
 # Aliases
 #
 
-alias ta='tmux attach -t'
-alias tad='tmux attach -d -t'
-alias ts='tmux new-session -s'
+alias ta='tmux attach -t -u'
+alias tad='tmux attach -d -t -u'
+alias ts='tmux new-session -s -u'
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
